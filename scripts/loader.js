ad8774c78dc71f598c772c73b6abf2b14db798a6
IvelinKrastev <ivelin.d.krastev@gmail.com>
2018-06-06 23:02:40 +0300

    Implemented the three recipes pages. Added the side menu and moved it into a new file. Added images for recipes. Also added one recipe for each category.

--- scripts/loader.js
+++ scripts/loader.js
@@ -6,7 +6,7 @@ function	loaderTimeout() {
 function	showPage() {
 	document.getElementById( "loader" ).style.display 		= "none";
   	document.getElementById( "headerLoad" ).style.display 	= "block";
-  	document.getElementById( "mainPage" ).style.display	= "block";
+  	document.getElementById( "mainPage" ).style.display		= "block";
   	document.getElementById( "footerLoad" ).style.display 	= "block";
 
   	hasActiveUser();
